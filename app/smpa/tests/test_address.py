6a5fad70141bc1cbba89e14ddc9c7e7309185ef0
Andy Beaumont <andy@andybeaumont.com>
2019-05-31 17:17:52 +0100

    Adds coverage and a docs page for tests

--- app/smpa/tests/test_address.py
+++ app/smpa/tests/test_address.py
@@ -10,6 +10,39 @@ def test_address_validates(address):
     assert address.validate() is None
 
 
-def test_address_save(address):
+####################################################################################################
+# Test the address service
+#
+# Some of these are marked as slow just because they access the database.
+#
+####################################################################################################
+
+@pytest.mark.slow
+def test_address_save_and_get(address):
     a = _addresses.save(address)
     assert _addresses.count() == 1
+    assert a.id is not None
+    assert _addresses.get(a.id) is not None
+    _addresses.delete(a)
+    assert _addresses.count() == 0
+
+
+@pytest.mark.slow
+def test_address_find(address):
+    a = _addresses.save(address)
+    assert len(_addresses.find(number='42')) > 0
+    _addresses.delete(a)
+    assert _addresses.count() == 0
+
+
+def test_new(address):
+    a = _addresses.new(
+        number="23",
+        property_name="The Big Building",
+        address_line_1="A Street",
+        address_line_2="Some district",
+        address_line_3="Something else",
+        town_city="London",
+        postcode="N1 1NN"
+    )
+    assert a.validate() is None
---
+++ docs/docs/tests.md
@@ -0,0 +1,45 @@
+# Tests
+
+Tests are written for pytest
+
+Run all tests...
+
+    pytest smpa
+
+Run all tests but stop when the first one fails...
+
+    pytest -x smpa
+
+Run all tests with log / print output...
+
+    pytest -s smpa
+
+Run all tests with log / print output but stop when the first one fails...
+
+    pytest -xs smpa
+
+Run all tests and drop into a debugger if any fail.
+
+    pytest --ipdb smpa
+
+
+## Test Coverage
+
+To generate a coverage report while running the tests...
+
+    pytest smpa --cov=smpa
+
+
+To send the coverage report to Codacy, requires `$CODACY_PROJECT_TOKEN` (currently stored in EnvKey) in your shell environment and the `codacy-coverage` package installed on your local machine.
+
+In the container's shell...
+
+    coverage xml
+
+On your local machine...
+
+    python-codacy-coverage -r coverage.xml
+
+To get a local HTML coverage report...
+
+    pytest smpa --cov=smpa --cov-report html:coverage
