aaff634b588c939738ff1250477d74fd1c287b4a
Andy Beaumont <andy@andybeaumont.com>
2019-08-09 14:15:27 +0100

    Allow S3 connections by region name for remotes.

--- app/smpa/helpers/s3.py
+++ app/smpa/helpers/s3.py
@@ -14,12 +14,20 @@ class S3:
     _BUCKET_NAME = 'smpa-documents'
 
     def __init__(self):
-        self.client = boto3.client(
-            's3',
-            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-            endpoint_url=os.environ.get('AWS_S3_ENDPOINT_URL')
-        )
+        if os.environ.get('AWS_S3_ENDPOINT_URL', None) is not None:
+            self.client = boto3.client(
+                's3',
+                aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+                aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+                endpoint_url=os.environ.get('AWS_S3_ENDPOINT_URL')
+            )
+        else:
+            self.client = boto3.client(
+                's3',
+                aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+                aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+                region_name=os.environ.get('AWS_REGION_NAME')
+            )
         self.bucket = self.client.create_bucket(Bucket=self._BUCKET_NAME)
 
     @property
