71ebc5afca41de88d1102cf12ea9673a169afd35
Andy Beaumont <andy@andybeaumont.com>
2019-08-10 09:10:58 +0100

    I think this authenticates against the staging db correctly.

--- app/smpa/db/documentdb/connection.py
+++ app/smpa/db/documentdb/connection.py
@@ -14,19 +14,13 @@ from pymongo import MongoClient
 class DocumentDB:
 
     def __init__(self, config):
-        import ipdb; ipdb.set_trace()
-        env = os.environ.get('SERVER_ENV')
-        if env == 'staging':
-            self._user = quote_plus(config.DOCUMENT_DB_SUPERUSER)
-            self._password = quote_plus(config.DOCUMENT_DB_SUPERPASS)
-        else:
-            self._user = quote_plus(config.DOCUMENT_DB_USER)
-            self._password = quote_plus(config.DOCUMENT_DB_PASSWORD)
+        self._user = quote_plus(config.DOCUMENT_DB_USER)
+        self._password = quote_plus(config.DOCUMENT_DB_PASSWORD)
 
         if config.DOCUMENT_DB_USER is not None:
             host = "{}:{}".format('127.0.0.1', int(config.DOCUMENT_DB_PORT))
             uri = f"mongodb://{self._user}:{self._password}@{host}/{config.DOCUMENT_DB_DB}"
-            uri_plus = f"{uri}?authSource=admin"
+            uri_plus = f"{uri}?authSource={config.DOCUMENT_DB_DB}"
             self.client = MongoClient(uri_plus)
         else:
             self.client = MongoClient(config.DOCUMENT_DB_HOST, int(config.DOCUMENT_DB_PORT))
